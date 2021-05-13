aa73c4d5db911297aaea2c08f1f59c723a6f0b40
Tom Herbert <tom@sipanda.io>
2021-05-13 14:29:25 -0700

    xdp: flow_tracker in samples
    Add sample code for flow_tracker which is a simple emonstration of
    PANDA in XDP.

---
+++ samples/flow_tracker/parser.c
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+/*
+ * Copyright (c) 2020, 2021 SiPanda Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <arpa/inet.h>
+#include <linux/types.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "siphash/siphash.h"
+
+/* Define protocol nodes that are used below */
+#include "panda/proto_nodes_def.h"
+
+/* Meta data functions for parser nodes. Use the canned templates
+ * for common metadata
+ */
+PANDA_METADATA_TEMP_ether(ether_metadata, panda_metadata_all)
+PANDA_METADATA_TEMP_ipv4(ipv4_metadata, panda_metadata_all)
+PANDA_METADATA_TEMP_ports(ports_metadata, panda_metadata_all)
+
+/* Parse nodes. Parse nodes are composed of the common PANDA Parser protocol
+ * nodes, metadata functions defined above, and protocol tables defined
+ * below
+ */
+
+PANDA_MAKE_PARSE_NODE(ether_node, panda_parse_ether, ether_metadata, NULL,
+		      ether_table);
+PANDA_MAKE_PARSE_NODE(ipv4_check_node, panda_parse_ip, NULL, NULL,
+		      ipv4_check_table);
+PANDA_MAKE_PARSE_NODE(ipv4_node, panda_parse_ipv4, ipv4_metadata, NULL,
+		      ipv4_table);
+PANDA_MAKE_LEAF_PARSE_NODE(ports_node, panda_parse_ports, ports_metadata, NULL);
+
+/* Protocol tables */
+
+PANDA_MAKE_PROTO_TABLE(ether_table,
+	{ __cpu_to_be16(ETH_P_IP), &ipv4_check_node },
+);
+
+PANDA_MAKE_PROTO_TABLE(ipv4_check_table,
+	{ 4, &ipv4_node },
+);
+
+PANDA_MAKE_PROTO_TABLE(ipv4_table,
+	{ IPPROTO_TCP, &ports_node },
+	{ IPPROTO_UDP, &ports_node },
+);
+
+PANDA_PARSER_ADD(panda_parser_simple_tuple, "PANDA parser for 5 tuple TCP/UDP",
+		 &ether_node);
+
---
+++ samples/flow_tracker/xdp_tmpl.bpf.c
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+/*
+ * Copyright (c) 2020, 2021 SiPanda Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#include "flow_tracker.h"
+#include "parser.xdp.h"
+
+#define PROG_MAP_ID 0xcafe
+
+struct bpf_elf_map SEC("maps") ctx_map = {
+	.type = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.size_key = sizeof(__u32),
+	.size_value = sizeof(struct panda_ctx),
+	.max_elem = 2,
+	.pinning = PIN_GLOBAL_NS,
+};
+struct bpf_elf_map SEC("maps") parsers = {
+	.type = BPF_MAP_TYPE_PROG_ARRAY,
+	.size_key = sizeof(__u32),
+	.size_value = sizeof(__u32),
+	.max_elem = 1,
+	.pinning = PIN_GLOBAL_NS,
+	.id = PROG_MAP_ID,
+};
+
+static __always_inline struct panda_ctx *panda_get_ctx(void)
+{
+	/* clang-10 has a bug if key == 0,
+	 * it generates bogus bytecodes.
+	 */
+	__u32 key = 1;
+
+	return bpf_map_lookup_elem(&ctx_map, &key);
+}
+
+SEC("0xcafe/0")
+int parser_prog(struct xdp_md *ctx)
+{
+	struct panda_ctx *parser_ctx = panda_get_ctx();
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	void *original = data;
+	int rc = PANDA_OKAY;
+
+	if (!parser_ctx)
+		return XDP_ABORTED;
+
+	/* >>> Invoke the specific panda parser */
+	rc = panda_parser_simple_tuple_panda_parse_ether_node(parser_ctx,
+					(const void **)&data, data_end, true);
+
+	if (rc != PANDA_OKAY && rc != PANDA_STOP_OKAY) {
+		bpf_xdp_adjust_head(ctx, -parser_ctx->offset);
+		return XDP_PASS;
+	}
+	if (parser_ctx->next != CODE_IGNORE) {
+		parser_ctx->offset += data - original;
+		bpf_xdp_adjust_head(ctx, data - original);
+		bpf_tail_call(ctx, &parsers, 0);
+	}
+
+	/* >>> Call processing user function here */
+	flow_track(parser_ctx->frame);
+
+	bpf_xdp_adjust_head(ctx, -parser_ctx->offset);
+	return XDP_PASS;
+}
+
+SEC("prog")
+int xdp_prog(struct xdp_md *ctx)
+{
+	struct panda_ctx *parser_ctx = panda_get_ctx();
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	void *original = data;
+	int rc = PANDA_OKAY;
+
+	if (!parser_ctx)
+		return XDP_ABORTED;
+
+	parser_ctx->metadata.frame_size = sizeof(parser_ctx->frame);
+	parser_ctx->metadata.max_frame_num = 0;
+	parser_ctx->frame_num = 0;
+	parser_ctx->next = CODE_IGNORE;
+
+	/* >>> Invoke the specific panda parser */
+	rc = panda_parser_simple_tuple_panda_parse_ether_node(parser_ctx,
+					(const void **)&data, data_end, false);
+
+	if (rc != PANDA_OKAY && rc != PANDA_STOP_OKAY)
+		return XDP_PASS;
+
+	if (parser_ctx->next != CODE_IGNORE) {
+		parser_ctx->offset = data - original;
+		bpf_xdp_adjust_head(ctx, parser_ctx->offset);
+		bpf_tail_call(ctx, &parsers, 0);
+	}
+
+	/* >>> Call processing user function here */
+	flow_track(parser_ctx->frame);
+
+	return XDP_PASS;
+}
+
+char __license[] SEC("license") = "GPL";
